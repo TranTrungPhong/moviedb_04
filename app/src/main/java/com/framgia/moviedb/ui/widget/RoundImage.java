68b1f7a747f1a812c149cf33e67acf837de5ea42
tuannt <tuanbaros@yahoo.com.vn>
2017-02-15 17:28:36 +0700

    retry-detail-screen

--- app/src/main/java/com/framgia/moviedb/ui/widget/RoundImage.java
+++ app/src/main/java/com/framgia/moviedb/ui/widget/RoundImage.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Bitmap.Config;
 import android.graphics.Canvas;
-import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.PorterDuff.Mode;
 import android.graphics.PorterDuffXfermode;
